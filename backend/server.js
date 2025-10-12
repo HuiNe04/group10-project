42e21ac0fe2a09b686067f072584c8f75d0bf98d
Khanh098788 <Vovankhanh937@gmail.com>
2025-10-12 20:26:08 +0700

    update

--- backend/server.js
+++ backend/server.js
@@ -3,19 +3,15 @@ const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const cors = require("cors");
 const userRoutes = require("./routes/user");
-
 dotenv.config();
-
 const app = express();
 app.use(express.json());
 app.use(cors());
-
 // Kết nối MongoDB
 mongoose
   .connect(process.env.MONGO_URI)
   .then(() => console.log("✅ Đã kết nối MongoDB Atlas thành công"))
   .catch((err) => console.error("❌ Lỗi kết nối MongoDB:", err));
-
 // Routes
 app.use("/api", userRoutes);
 
