cc2b70efb1495132bcbdb30a6efa0ce2a04fb87b
Khanh098788 <Vovankhanh937@gmail.com>
2025-10-21 08:37:39 +0700

    add auth  login , logout

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,8 @@ const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const cors = require("cors");
 const userRoutes = require("./routes/user");
+const authRoutes = require("./routes/auth");
+
 dotenv.config();
 
 const app = express();
@@ -15,6 +17,7 @@ mongoose
   .catch((err) => console.error(" Lỗi kết nối MongoDB:", err));
 // Routes
 app.use("/api", userRoutes);
+app.use("/api/auth", authRoutes);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(` Server running on port ${PORT}`))
